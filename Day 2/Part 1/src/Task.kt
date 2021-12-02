9041a7d7ae55cab16c6a2d11373aa0c4a6f64bd2
Marc Bouvier <m.bouvier.dev@gmail.com>
2021-12-02 11:52:40 +0100

    Day 2 - part 1

---
+++ Day 2/Part 1/src/Task.kt
@@ -0,0 +1,3 @@
+fun pilotSubmarine(commands: Array<String>): Int {
+    return -1 // replace with the result of your solution
+}
---
+++ Day 2/Part 1/test/Tests.kt
@@ -0,0 +1,87 @@
+import org.junit.Assert
+import org.junit.Test
+
+class Test {
+
+    @Test
+    fun testInput01() {
+        assertSolutionWorks("01", 0, arrayOf())
+    }
+
+    @Test
+    fun testInput02() {
+        assertSolutionWorks("02", 0, arrayOf("forward 5"))
+    }
+
+    @Test
+    fun testInput03() {
+        assertSolutionWorks("03", 0, arrayOf("down 5"))
+    }
+
+    @Test
+    fun testInput04() {
+        assertSolutionWorks("04", 1, arrayOf("forward 1", "down 1"))
+    }
+
+    @Test
+    fun testInput05() {
+        assertSolutionWorks("05", 10, arrayOf("forward 5", "forward 5", "down 1"))
+    }
+
+    @Test
+    fun testInput06() {
+        assertSolutionWorks("06", 5, arrayOf("forward 1", "down 5"))
+    }
+
+    @Test
+    fun testInput07() {
+        assertSolutionWorks("07", 0, arrayOf("forward 1", "down 5", "up 5"))
+    }
+
+    @Test
+    fun testInput08() {
+        assertSolutionWorks("08", 25, arrayOf("down 5", "forward 5"))
+    }
+
+    @Test
+    fun testInput09() {
+        assertSolutionWorks("09", 5, arrayOf("down 5", "up 4", "forward 5"))
+    }
+
+    @Test
+    fun testInput10() {
+
+        assertSolutionWorks(
+            "10", 150, arrayOf(
+                "forward 5", "down 5", "forward 8",
+                "up 3", "down 8", "forward 2",
+            )
+        )
+    }
+
+    @Test
+    fun testInput11() {
+        val fullInputText = javaClass.getResource("full_input.txt").readText()
+        val fullInput = fullInputText
+            .split("\n")
+            .map(String::trim)
+            .take(500) // I take half the file content in order to not spoil the Calendar result
+            .toTypedArray()
+        val actual = pilotSubmarine(fullInput)
+        assertSolutionWorksWithPuzzleInputSource(actual, 435335)
+    }
+
+    private fun assertSolutionWorks(testNumber: String, expected: Int, measurements: Array<String>) {
+        val actual = pilotSubmarine(measurements)
+        val formattedMeasurements = measurements.joinToString(", ", "[", "]")
+        Assert.assertEquals(
+            "Test #$testNumber : Expected $expected, but was $actual. Measures provided were : $formattedMeasurements",
+            expected,
+            actual
+        )
+    }
+
+    private fun assertSolutionWorksWithPuzzleInputSource(actual: Int, expected: Int) {
+        Assert.assertEquals("Wrong answer with data from half the puzzle input file content", expected, actual)
+    }
+}
