e2c40c4537976638141636984277a3fc31fa7bab
Marc Bouvier <m.bouvier.dev@gmail.com>
2021-12-03 11:33:16 +0100

    Day 3 Part 1

--- Day 3/Part 1/test/Tests.kt
+++ Day 3/Part 1/test/Tests.kt
@@ -5,23 +5,94 @@ class Test {
 
     @Test
     fun testInput01() {
-        assertSolutionWorks("01", 0, arrayOf())
+        // Gamma   = 1 => 1
+        // Epsilon = 0 => 0
+        assertSolutionWorks(
+            "01", 0, arrayOf(
+                "0",
+                "1",
+                "1"
+            )
+        )
     }
 
     @Test
-    fun testInput12() {
+    fun testInput02() {
+        // Gamma   = 0 => 0
+        // Epsilon = 1 => 1
+        assertSolutionWorks(
+            "02", 0, arrayOf(
+                "0",
+                "0",
+                "1"
+            )
+        )
+    }
+
+    @Test
+    fun testInput03() {
+        // Gamma   = 10 => 2
+        // Epsilon = 01 => 1
+        assertSolutionWorks(
+            "03", 2, arrayOf(
+                "01",
+                "10",
+                "10"
+            )
+        )
+    }
+
+    @Test
+    fun testInput04() {
+        // Gamma   = 10110 => 22
+        // Epsilon = 01001 => 9
+        assertSolutionWorks(
+            "04", 198, arrayOf(
+                "00100",
+                "11110",
+                "10110",
+                "10111",
+                "10101",
+                "01111",
+                "00111",
+                "11100",
+                "10000",
+                "11001",
+                "00010",
+                "01010"
+            )
+        )
+    }
+
+    @Test
+    fun testInput05() {
+        // Gamma   = 000000000001 => 1
+        // Epsilon = 111111111110 => 4094
+        //       >2048 >128    8
+        assertSolutionWorks(
+            "06", 4094, arrayOf(
+                "000000000000",
+                "111111111111",
+                "000000000001"
+            )
+        )
+    }
+
+
+    @Test
+    fun testInput06() {
         val fullInputText = javaClass.getResource("full_input.txt").readText()
         val fullInput = fullInputText
             .split("\n")
             .map(String::trim)
             .take(500) // I take half the file content in order to not spoil the Calendar result
             .toTypedArray()
-        val actual = doSomething(fullInput)
-        assertSolutionWorksWithPuzzleInputSource(actual, 222935578)
+        val actual = decodePowerConsumption(fullInput)
+        assertSolutionWorksWithPuzzleInputSource(actual, 4192166)
     }
 
     private fun assertSolutionWorks(testNumber: String, expected: Int, measurements: Array<String>) {
-        val actual = doSomething(measurements)
+        val actual = decodePowerConsumption(measurements)
         val formattedMeasurements = measurements.joinToString(", ", "[", "]")
         Assert.assertEquals(
             "Test #$testNumber : Expected $expected, but was $actual. Measures provided were : $formattedMeasurements",
